c5c24c6f39401dc83624f3d41cb78ed0cf63aa05
gouder hicham <gouderhicham@gmail.com>
2026-08-29 14:46:25 +0100

    fix(prisma): configure wasm engineType and @prisma/client/wasm for Cloudflare Workers

--- front-end/src/server/db.ts
+++ front-end/src/server/db.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/wasm";
 
 /**
  * Prisma on Cloudflare Workers.
